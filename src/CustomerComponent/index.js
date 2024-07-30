34db8c67e566572f4a72ac84229e50eb14b859d2
nannigeri <annigeri.n@northeastern.edu>
2024-07-30 11:22:41 -0400

    design for customer cards

--- src/CustomerComponent/index.js
+++ src/CustomerComponent/index.js
@@ -2,15 +2,24 @@ import "./index.css"
 import customers from  "./customers.json";
 
 function CustomerComponent() {
-    return (<div>
-        <div>Customers</div>
-        <ul>
-        {customers.map(client => <li>
-            <div>{client.name}</div>
-            <div>{client.email}</div>
-            <div>{client.password}</div>
-            </li>)}
-        </ul>
+    return (<div> 
+        <div>
+        <div class= 'title-font'>Customers</div>
+        {customers.map(client => <div class="spacing"><div class='card side-by-side'>
+            <div class = "spacing-side" >
+                <div class= "category"> 
+               Name
+               </div> {client.name}
+                </div>
+            <div class = "spacing-side"><div class= "category"> 
+               Email
+               </div> {client.email}</div>
+            <div class= "spacing-side"><div class= "category"> 
+               Password
+               </div> {client.password}</div>
+            </div>
+            </div>)}
+        </div>
        
         
     </div>);
