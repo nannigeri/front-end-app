e425ffb7131807638d2a30464e37221291f843f9
nannigeri <annigeri.n@northeastern.edu>
2024-07-30 10:49:09 -0400

    static list of customers working

--- src/CustomerComponent/index.js
+++ src/CustomerComponent/index.js
@@ -1,7 +1,19 @@
 import "./index.css"
+import customers from  "./customers.json";
 
 function CustomerComponent() {
     return (<div>
+        <div>Customers</div>
+        <ul>
+        {customers.map(client => <li>
+            <div>{client.name}</div>
+            <div>{client.email}</div>
+            <div>{client.password}</div>
+            </li>)}
+        </ul>
+       
         
     </div>);
 }
+
+export default CustomerComponent;
