6a55babdaba654a77e554a0ed03023aa84f53d9c
nannigeri <annigeri.n@northeastern.edu>
2024-07-31 13:13:01 -0400

    removing static data from my customer file

--- src/CustomerComponent/index.js
+++ src/CustomerComponent/index.js
@@ -1,39 +1,39 @@
 import "./index.css"
-import {useState} from 'react';
+import { useState } from 'react';
 
 
 const CustomerComponent = (props) => {
 
-  return (<div> 
+    return (<div>
         <div>
-        <div class= 'title-font'>Customer List</div>
-        
-        <div class = "card-spacing">
+            <div class='title-font'>Customer List</div>
 
-        
-        
-        {props.data.map(client =><div key ={client.id} class={(client.id === props.formObject.id)? " clicked-card": "card "} onClick = {() => props.handleRowClick(client)}>
-            
-            <div class= "side-by-side spacing">
-            
-            <div class = "spacing-side" >
-                <div class= "category"> 
-               Name
-               </div> 
-               {client.name}
-                </div>
-            <div class = "spacing-side"><div class= "category"> 
-               Email
-               </div> {client.email}</div>
-            <div class= "spacing-side"><div class= "category"> 
-               Password
-               </div> {client.password}</div>
-            </div>
-            </div>)}
+            <div class="card-spacing">
+
+
+
+                {props.data.map(client => <div key={client.id} class={(client.id === props.formObject.id) ? " clicked-card" : "card "} onClick={() => props.handleRowClick(client)}>
+
+                    <div class="side-by-side spacing">
+
+                        <div class="spacing-side" >
+                            <div class="category">
+                                Name
+                            </div>
+                            {client.name}
+                        </div>
+                        <div class="spacing-side"><div class="category">
+                            Email
+                        </div> {client.email}</div>
+                        <div class="spacing-side"><div class="category">
+                            Password
+                        </div> {client.password}</div>
+                    </div>
+                </div>)}
             </div>
         </div>
-       
-        
+
+
     </div>);
 }
 
